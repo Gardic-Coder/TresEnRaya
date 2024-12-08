467e1aaad0d59ba805dbc648487028c829054da5
SherLait <keinerperezalguaca@gmail.com>
2024-12-07 23:41:09 -0400

    Si funciona, me juego Persona 3

--- Datos/DataAccess.cpp
+++ Datos/DataAccess.cpp
@@ -1,2 +1,58 @@
 // Datos/DataAccess.h
 #include "DataAccess.h"
+#include <fstream.h>
+
+using namespace std;
+
+void DataAccess::guardarR(TableroDeJuego tablero, vector<int> resultados[][] ){
+	//Guarda los valores del tablero de la ultima partida
+	
+	vector<int> resultados [s][s];
+	
+	for (int i = 0; i <= s; i++){
+		
+		for (int j = 0; j <= s; j++ ){
+			
+			tablero[i][j] = resultados[i][j];
+		}
+		
+	}
+	
+	return resultados;
+};
+
+void DataAccess::crearF(vector<int> resultados[][]){
+	//Escribe los resultados en un .txt
+	
+	int n = 3;
+	
+	ofstream hojaResult("Datos de partida.txt");
+	
+	hojaResult.open();
+	
+	hojaResult << "Resultados del juego:" << endl;
+	
+	for (int i = 0; i < n; i++){
+		
+		for (int j = 0; j < n; j++){
+			
+			switch(resultados[i][j]){
+				case 1: hojaResult << "X" << "\n";
+						break;
+				case 2: hojaResult << "O" << "\n";
+						break;
+				default: hojaResult << "-" << "\n";
+						break;
+			}	
+			
+		}
+		
+		hojaResult << endl;
+		
+	}
+	
+	hojaResult.close();
+	
+	return 0;
+	
+};
--- Datos/DataAccess.h
+++ Datos/DataAccess.h
@@ -1,3 +1,18 @@
 // Datos/DataAccess.h
 #pragma once
+#include <vector>
 
+using namespace std;
+
+
+class DatosdeJuego{
+	
+	private:
+		vector<int> resultados [3][3];
+		
+	public:
+		DatosdeJuego();
+		void guardarR(TableroDeJuego tablero, vector<int> resultados[][]);
+		void crearF(vector<string> resultados);
+		
+};
